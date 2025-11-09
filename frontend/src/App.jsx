15c8ca0265fa57e780bd4ad4d31825cc1ea053fd
Guntawit Anurakboonying <guntawit.anu@student.mahidol.edu>
2025-11-09 22:26:43 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,32 @@
 // Heart is here
 
+
 import { useEffect, useState } from 'react'
 
+const App = () => {
+    const [hasLiked, setHasLiked] = useState(false);
+
+    return (
+        <div className="member-container">
+            <Member name="Ben" />
+            <Member name="Plub" />
+            <Member name="Heart" />
+        </div>
+    )
+}
+
+const Member = ({ name }) => {
+    return (
+        <div className="member-box">
+        <h2>{name}</h2>
+            <button onClick={() => setHasLiked(true)}>
+                Like
+            </button>
+        </div>
+    )
+}
+
+/*
 function App() {
     const [message, setMessage] = useState("")
 
@@ -15,8 +40,13 @@ function App() {
         <div>
             <h1>React + Vite + FastAPI</h1>
             <p>{message}</p>
+            <Member name="Ben" />
+            <Member name="Heart" />
+            <Member name="Plub  " />
+
         </div>
     )
 }
+*/
 
 export default App
